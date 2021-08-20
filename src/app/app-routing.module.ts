421ec572d14ea01db1004eac340da14a3ba5bc46
SecretProgrammer10 <raj.science100@gmail.com>
2021-08-21 02:44:02 +0530

    Added Basic User Login Functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,8 @@ const routes : Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule , RouterModule.forRoot(routes)
+    CommonModule, 
+    RouterModule.forRoot(routes)
   ],
   exports: [ RouterModule ]
 })
