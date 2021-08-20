298810932572892c135333e8442802765c751bb4
Dibyendu <dibyenduganguly.48@gmail.com>
2021-08-21 03:18:44 +0530

    login user added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../service/user.service';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,44 @@ import { FormGroup } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
+  userLoginForm !: FormGroup;
   
+  userUsername !: FormControl;
+  userPassword !: FormControl;
+
+  userLoginFormSubmitted = false;
+
+  userResponseString?: String;
+  userResponseStatus?: Number;
   
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+
+    this.userUsername = new FormControl(null,[Validators.required]);
+    this.userPassword = new FormControl(null,[Validators.required]);
+
+    this.userLoginForm = new FormGroup({
+      'username' : this.userUsername,
+      'pass' : this.userPassword
+    });
     
+  }
+
+  userLogin(username:string,pass:string){
+    console.log("login called");
+    this.userService.loginUser(username,pass).subscribe(
+      data => {
+        console.log(data)
+      },
+      error => {
+        console.log(error)
+      }
+    )
+    this.userLoginFormSubmitted = true;
+    this.userLoginForm.reset();
   }
 
 }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Users } from '../models/user.model';
 
@@ -16,9 +16,12 @@ export class UserService {
     return this.http.post(`${this.baseUrl}`+'/register-user',user,{responseType: 'text', observe: 'response'});
   }
 
-  // loginUser(user: Users):Observable<HttpResponse<String>>{
-  //   return this.http.post(`${this.baseUrl}`+'/login-user',user,{responseType: 'text', observe: 'response'});
-  // }
+  loginUser(username: string, pass: string):Observable<HttpResponse<String>>{
+    let params = new HttpParams()
+    .set('username', username)
+    .set('pass', pass);
+    return this.http.post(`${this.baseUrl}`+'/login-user',params,{responseType: 'text', observe: 'response'});
+  }
   
 
 }
