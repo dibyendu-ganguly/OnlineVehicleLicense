e4d4318400834eeb42cad911e70533438b6bb504
Dibyendu <dibyenduganguly.48@gmail.com>
2021-08-22 03:44:25 +0530

    logout added

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -11,20 +11,39 @@ export class NavComponent implements OnInit {
 
   constructor(
     private router: Router
-  ) { }
+  ) {  }
 
-  isLoggedIn : boolean = false;
-  userName ?: String; 
+  isLoggedIn: boolean = false;
+  userName?: String;
 
   ngOnInit(): void {
-
+    
     if (localStorage.getItem('UserName') != null || sessionStorage.getItem('UserName') != null) {
       this.isLoggedIn = true;
-      this.userName = localStorage.getItem('UserName')?.toString();
-    } else {
+      if (localStorage.getItem('UserName')?.toString() != null) {
+        this.userName = localStorage.getItem('UserName')?.toString();
+      }
+      else if (sessionStorage.getItem('UserName')?.toString() != null) {
+        this.userName = sessionStorage.getItem('UserName')?.toString();
+      }
+    }
+
+
+  }
+
+  logout() {
+    if (localStorage.getItem('UserName') != null || sessionStorage.getItem('UserName') != null) {
+      localStorage.removeItem('UserName');
+      sessionStorage.removeItem('UserName');
       this.isLoggedIn = false;
     }
 
+    if (localStorage.getItem('token') != null || sessionStorage.getItem('token') != null) {
+      sessionStorage.removeItem('token');
+      localStorage.removeItem('token');
+      //this.isLoggedIn = false;
+    }
+    window.location.reload();
   }
 
 }
