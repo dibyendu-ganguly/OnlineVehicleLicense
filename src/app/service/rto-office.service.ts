ff84cad14418c8885ee3ce8f2df7b9546f758216
SecretProgrammer10 <raj.science100@gmail.com>
2021-08-24 14:27:41 +0530

    Add Rto Office Functionality Done

--- src/app/service/rto-office.service.ts
+++ src/app/service/rto-office.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RtoOffice } from '../models/rto-office.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RtoOfficeService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:6969/ovl/rtoOffice';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  addRtoOffice(rtoOffice: RtoOffice):Observable<HttpResponse<String>>{
+    return this.http.post(`${this.baseUrl}`+`/add-rto-office`, rtoOffice, {responseType: 'text', observe: 'response'});
+  }
+
 }
